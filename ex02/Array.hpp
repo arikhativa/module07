e07950bbb9b99897cc387644c2b5fc1c11147553
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2023-10-03 15:29:47 +0200

    rdy

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,124 +6,103 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 11:42:23 by yrabby            #+#    #+#             */
-/*   Updated: 2023/06/19 12:40:23 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/10/03 15:29:28 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
-# define ARRAY_HPP
+#define ARRAY_HPP
 
-# include <stdexcept>
-# include <iostream>
+#include <iostream>
+#include <stdexcept>
 
-template < typename T >
-class Array
+template <typename T> class Array
 {
-	public:
+  private:
+    T *_array;
+    ::size_t _size;
 
-	/*
-	** ------------------------------- CONSTRUCTOR --------------------------------
-	*/
+  public:
+    /*
+    ** ------------------------------- CONSTRUCTOR --------------------------------
+    */
 
-		Array()
-			: _array(NULL), _size(0)
-		{
-		};
-		
-		explicit Array(unsigned int n)
-			: _array(new T[n]), _size(n)
-		{
+    Array() : _array(NULL), _size(0){};
 
-		};
+    explicit Array(unsigned int n)
+        : _array(new T[n]), _size(n){
 
-		Array( Array const & src )
-			: _array(new T[src.size()]), _size(src.size())
-		{
-			std::size_t	i = 0;
+                            };
 
-			while (i < src.size())
-			{
-				_array[i] = src[i];
-				i++;
-			}
-		};
+    Array(Array const &src) : _array(new T[src.size()]), _size(src.size())
+    {
+        for (::size_t i = 0; i < size(); ++i)
+            _array[i] = src[i];
+    };
 
-	/*
-	** -------------------------------- DESTRUCTOR --------------------------------
-	*/
+    /*
+    ** -------------------------------- DESTRUCTOR --------------------------------
+    */
 
-		~Array()
-		{
-			delete [] _array;
-			_size = 0;
-		};
+    ~Array()
+    {
+        delete[] _array;
+        _size = 0;
+    };
 
-	/*
-	** --------------------------------- OVERLOAD ---------------------------------
-	*/
+    /*
+    ** --------------------------------- OVERLOAD ---------------------------------
+    */
 
-		T		&operator[](std::size_t i)
-		{
-			if (i < 0 || i >= _size)
-				throw std::out_of_range("Index out of range");
-			return _array[i];
-		};
+    T &operator[](::size_t i)
+    {
+        if (i < 0 || i >= size())
+            throw std::out_of_range("Index out of range");
+        return _array[i];
+    };
 
-		const T		&operator[](std::size_t i) const
-		{
-			if (i < 0 || i >= _size)
-				throw std::out_of_range("Index out of range");
-			return _array[i];
-		};
+    const T &operator[](::size_t i) const
+    {
+        if (i < 0 || i >= size())
+            throw std::out_of_range("Index out of range");
+        return _array[i];
+    };
 
-		Array &		operator=( Array const & rhs )
-		{
-			if ( this != &rhs )
-			{
-				if (size() != rhs.size())
-				{
-					delete [] _array;
-					_size = rhs.size();
-					_array = new T[_size];
-				}
+    Array &operator=(Array const &rhs)
+    {
+        if (this != &rhs)
+        {
+            delete[] _array;
 
-				std::size_t	i = 0;
+            _size = rhs.size();
+            _array = new T[size()];
 
-				while (i < rhs.size())
-				{
-					_array[i] = rhs[i];
-					i++;
-				}
-			}
-			return *this;
-		};
+            for (::size_t i = 0; i < size(); ++i)
+                _array[i] = rhs[i];
+        }
+        return *this;
+    };
 
-	/*
-	** --------------------------------- METHODS ----------------------------------
-	*/
-	std::size_t	size(void) const
-	{
-		return _size;
-	};
-
-	private:
-		T			*_array;
-		std::size_t	_size;
-
+    /*
+    ** --------------------------------- METHODS ----------------------------------
+    */
+    ::size_t size(void) const
+    {
+        return _size;
+    };
 };
 
-template <typename T>
-std::ostream &			operator<<( std::ostream & o, Array<T> const & array )
+template <typename T> std::ostream &operator<<(std::ostream &o, Array<T> const &array)
 {
-	std::cout << "[";
-	for (std::size_t i = 0; i < array.size(); i++)
-	{
-		o << array[i];
-		if (i + 1 < array.size())
-			std::cout << ", ";
-	}
-	std::cout << "]";
-	return o;
+    std::cout << "[";
+    for (::size_t i = 0; i < array.size(); i++)
+    {
+        o << array[i];
+        if (i + 1 < array.size())
+            std::cout << ", ";
+    }
+    std::cout << "]";
+    return o;
 }
 
 #endif /* ********************************************************* ARRAY_HPP */
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,108 +6,129 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 10:45:46 by yrabby            #+#    #+#             */
-/*   Updated: 2023/06/19 12:41:37 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/10/03 15:28:28 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
-# include <iostream>
 
-class Data
+#include <iostream>
+
+struct Data
 {
-	public:
-		Data() : _str(""), _int(0) {};
-		Data(const std::string &str, int i) : _str(str), _int(i) {};
-		~Data() {};
-		std::string _str;
-		int			_int;
+    Data() : _str(""), _int(0){};
+    Data(const std::string &str, int i) : _str(str), _int(i){};
 
+    std::string _str;
+    int _int;
 };
 
-std::ostream		&operator<<( std::ostream & o, const Data & data )
+std::ostream &operator<<(std::ostream &o, const Data &data)
 {
-	o << "'" << data._str << "' - " << data._int;
-	return o;
+    o << "{" << data._str << ", " << data._int << "}";
+    return o;
 }
 
-std::ostream		&operator<<( std::ostream & o, const Data * data )
+std::ostream &operator<<(std::ostream &o, const Data *data)
 {
-	o << *data;
-	return o;
+    o << *data;
+    return o;
 }
 
 int main()
 {
-	{
-		std::cout << "ctor()" << std::endl;
-		Array<int> a;
-		std::cout << a << std::endl;
-		std::cout << std::endl << " ------------------------------- "  << std::endl << std::endl;
-	}
-	{
-		std::cout << "ctor(5)" << std::endl;
-		Array<int> a(5);
+    {
+        std::cout << "ctor()" << std::endl;
+        Array<int> a;
+        std::cout << a << std::endl;
+        std::cout << std::endl << " ------------------------------- " << std::endl << std::endl;
+    }
+    {
+        std::cout << "ctor(5)" << std::endl;
+        Array<int> a(5);
 
-		a[0] = 1;
-		a[1] = 2;
-		a[2] = 3;
-		a[3] = 4;
-		a[4] = 5;
-		std::cout << a << std::endl;
-		std::cout << std::endl << " ------------------------------- "  << std::endl << std::endl;
-	}
-	{
-		std::cout << "copy ctor()" << std::endl;
-		Array<int> a(5);
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        a[3] = 4;
+        a[4] = 5;
+        std::cout << a << std::endl;
+        std::cout << std::endl << " ------------------------------- " << std::endl << std::endl;
+    }
+    {
+        std::cout << "copy ctor()" << std::endl;
+        Array<int> a(5);
 
-		a[0] = 10;
-		a[1] = 20;
-		a[2] = 30;
-		a[3] = 40;
-		a[4] = 50;
-		Array<int> b(a);
-		b[0] = -1;
-		std::cout << a << std::endl;
-		std::cout << b << std::endl;
-		std::cout << std::endl << " ------------------------------- "  << std::endl << std::endl;
-	}
-	{
-		std::cout << "Data{string, int}" << std::endl;
-		Array<Data> a(2);
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 30;
+        a[3] = 40;
+        a[4] = 50;
+        Array<int> b(a);
+        b[0] = -1;
+        std::cout << a << std::endl;
+        std::cout << b << std::endl;
+        std::cout << std::endl << " ------------------------------- " << std::endl << std::endl;
+    }
+    {
+        std::cout << "Data{string, int}" << std::endl;
+        Array<Data> a(2);
 
-		a[0] = Data("hey", 1);
-		a[1] = Data("dudu", 2);
+        a[0] = Data("hey", 1);
+        a[1] = Data("dudu", 2);
 
-		std::cout << a << std::endl;
-		std::cout << std::endl << " ------------------------------- "  << std::endl << std::endl;
-	}
-	{
-		std::cout << "Array<Data*>*" << std::endl;
-		Array<Data *> *ptr = new Array<Data *>(2);
-		Array<Data *> &a = *ptr;
+        std::cout << a << std::endl;
+        std::cout << std::endl << " ------------------------------- " << std::endl << std::endl;
+    }
+    {
+        std::cout << "Array<Data*>*" << std::endl;
+        Array<Data *> *ptr = new Array<Data *>(2);
+        Array<Data *> &a = *ptr;
 
-		a[0] = new Data("hey", 1);
-		a[1] = new Data("dudu", 2);
-		std::cout << a << std::endl;
-		for (std::size_t i = 0; i < a.size(); i++)
-			delete a[i];
-		delete ptr;
-		std::cout << std::endl << " ------------------------------- "  << std::endl << std::endl;
-	}
-	{
-		std::cout << "Out of bound" << std::endl;
-		Array<int> a;
-		try
-		{
-			a[0];
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+        a[0] = new Data("hey", 1);
+        a[1] = new Data("dudu", 2);
+        std::cout << a << std::endl;
+        for (::size_t i = 0; i < a.size(); i++)
+            delete a[i];
+        delete ptr;
+        std::cout << std::endl << " ------------------------------- " << std::endl << std::endl;
+    }
+    {
+        std::cout << "Out of bound" << std::endl;
+        Array<int> a;
+        try
+        {
+            a[0];
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        try
+        {
+            a[500];
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
 
-		std::cout << std::endl << " ------------------------------- "  << std::endl << std::endl;
-	}
-	
-	return 0;
+        std::cout << std::endl << " ------------------------------- " << std::endl << std::endl;
+    }
+    {
+        std::cout << "const" << std::endl;
+        Array<int> a(2);
+        a[0] = 1;
+        a[1] = 2;
+        const Array<int> b(a);
+
+        a[0] = 10;
+        // b[0] = 1;
+        std::cout << a << std::endl;
+        std::cout << b << std::endl;
+
+        std::cout << std::endl << " ------------------------------- " << std::endl << std::endl;
+    }
+
+    return 0;
 }
